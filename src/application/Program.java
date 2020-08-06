b353166c0302330e2ae39d4e4889272ff320910b
Fabio Celis <fabiohcelis@gmail.com>
2020-08-06 21:30:32 +0100

    Acrescentada mensagem!

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,8 +1,17 @@
 package application;
 
+import java.util.Scanner;
+
 public class Program {
 
     public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Digite uma ideia: ");
+        String ideia = sc.nextLine();
+
+        System.out.println(ideia);
         
     }
 }
